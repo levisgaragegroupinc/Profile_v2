596b8099a0afddd82ebdba70f9e1cc6f3d3b92fb
Travis Levi B <tbateswsu@gmail.com>
2022-07-14 11:55:57 -0700

    Code: First commit. Basic file structure.

---
+++ App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+// component import example
+// import BucketList from "./components/BucketList";
+
+function App() {
+  return (
+    /* EXAMPLE OF USE
+    <div className="bucket-app">
+      <BucketList />
+    </div>
+    */
+  );
+}
+
+export default App;
