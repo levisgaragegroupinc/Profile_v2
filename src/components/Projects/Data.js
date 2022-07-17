fe2c3fba4875b16c4600ce3323c0efd39f39f93e
Travis Bates <tbateswsu@gmail.com>
2022-07-16 19:24:16 -0700

    debug: Anchor tags, remove # from in front of anchor name.

--- src/components/Projects/Data.js
+++ src/components/Projects/Data.js
@@ -5,6 +5,7 @@ const projectPortfolio = [
     project_title: "Technology Blog",
     description:
       "Blog site that allows users to register, make posts, and comment.",
+    anchor_tag: "techblog",
     repo_url: "https://github.com/levisgaragegroupinc/mvc_tech-blog",
     app_url: "https://mvc-technology-blog.herokuapp.com/",
     code_stack: [
@@ -27,6 +28,7 @@ const projectPortfolio = [
     project_title: "Knicknacks",
     description: "A communit jobs board for everyone.",
     repo_url: "https://github.com/levisgaragegroupinc/KnickKnacks",
+    anchor_tag: "knickknacks",
     app_url: "https://knickknacks-ondemand.herokuapp.com/",
     code_stack: [
       "HTML",
@@ -48,6 +50,7 @@ const projectPortfolio = [
     project_image: "wx_01.png",
     project_title: "Open Weather App",
     description: "A 5-day forecast weather app. Save locations.",
+    anchor_tag: "weather",
     repo_url: "https://github.com/levisgaragegroupinc/open-weather-app",
     app_url: "https://levisgaragegroupinc.github.io/open-weather-app/",
     code_stack: ["HTML", "CSS", "Javascript", "Moment.js", "OpenWeather API"],
@@ -58,6 +61,7 @@ const projectPortfolio = [
     project_title: "Quiz App",
     description:
       "An app that tests users knowledge of the Javascript programming language.",
+    anchor_tag: "quiz",
     repo_url: "https://github.com/levisgaragegroupinc/Programming-Quiz",
     app_url: "https://levisgaragegroupinc.github.io/Programming-Quiz/",
     code_stack: ["HTML", "CSS", "Javascript"],
@@ -67,6 +71,7 @@ const projectPortfolio = [
     project_image: "translation_01.png",
     project_title: "Translation App",
     description: "Translate sentences from English into 17 other languages.",
+    anchor_tag: "translation",
     repo_url: "https://github.com/levisgaragegroupinc/translation_app",
     app_url: "https://levisgaragegroupinc.github.io/translation_app/",
     code_stack: [
@@ -86,6 +91,7 @@ const projectPortfolio = [
     project_title: "Notes",
     description:
       "A simple note taker. Add and delete notes. Notes include title.",
+    anchor_tag: "notes",
     repo_url: "https://github.com/levisgaragegroupinc/note-keeper",
     app_url: "https://glacial-plains-24658.herokuapp.com/notes",
     code_stack: [
