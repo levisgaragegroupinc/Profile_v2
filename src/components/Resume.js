61e72b5feee8491ac1ee6bfa8e1fbd4c391a7406
Travis Bates <tbateswsu@gmail.com>
2022-07-19 15:35:50 -0700

    feat: Building resume page, added resume file and img.

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,7 +1,8 @@
 import React from "react";
 import Divider from "./Dividers/Divider";
 import Heading from "./Dividers/Heading";
-
+import pdf from "../assets/Resume-01.pdf";
+let imagePath = require(`../assets/resume-img.png`);
 const title = "Resume";
 
 function Resume() {
@@ -9,6 +10,9 @@ function Resume() {
     <div>
       <Divider />
       <Heading title={title} />
+      <a href={pdf} target="_blank" rel="noreferrer">
+        <img src={imagePath} alt="Resume."></img>
+      </a>
     </div>
   );
 }
