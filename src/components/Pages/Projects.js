fbcaa10de5150d95cc5b80937bef228039bae832
Travis Levi B <tbateswsu@gmail.com>
2022-07-20 16:31:00 -0700

    feat: added name and occupation to project page header.

--- src/components/Pages/Projects.js
+++ src/components/Pages/Projects.js
@@ -7,10 +7,16 @@ import DropDown from "../Buttons/DropDown";
 
 const title = "Projects";
 const labelBtnDropdown = "Project List";
+const username = "Travis Bates";
+const occupation = "Software Developer";
 
 function Projects() {
   return (
     <div>
+      <div className="uk-flex uk-flex-column uk-flex-middle">
+        <h3 className="uk-padding-small uk-margin-remove">{username}</h3>
+        <h4 className="uk-margin-remove">{occupation}</h4>
+      </div>
       <Divider />
       <Heading title={title} />
       <DropDown btnLabel={labelBtnDropdown} projectData={projectPortfolio} />
