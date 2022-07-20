62f6c9c1af9e8db27836b2939c0d9c9700013438
Travis Levi B <tbateswsu@gmail.com>
2022-07-20 16:57:26 -0700

    feat: added stack experience list to the resume page.

--- src/components/Pages/Resume.js
+++ src/components/Pages/Resume.js
@@ -5,9 +5,33 @@ import resumePdf from "../../assets/Resume-01.pdf";
 let imagePath = require(`../../assets/resume-img.png`);
 const title = "Resume";
 
+const listname = "Stack Experience";
+const stacklist = [
+  "HTML",
+  "CSS",
+  "Javascript",
+  "React",
+  "JQuery",
+  "Handlebars",
+  "Express",
+  "Node",
+  "MySQL",
+  "JAWS DB",
+  "MongoDB",
+  "GraphQL",
+];
+
 function Resume() {
   return (
     <div>
+      <div className="uk-flex uk-flex-column uk-flex-middle">
+        <h3 className="uk-padding-small uk-margin-remove">{listname}</h3>
+        <ul className="uk-align-center uk-flex uk-flex-wrap uk-flex-center">
+          {stacklist.map((code) => {
+            return <li className="uk-padding-small">{code}</li>;
+          })}
+        </ul>
+      </div>
       <Divider />
       <Heading title={title} />
       <div className="uk-flex uk-flex-center">
@@ -19,9 +43,11 @@ function Resume() {
           uk-icon="icon: download; ratio: 2"
         ></a>
       </div>
-      <a href={resumePdf} target="_blank" rel="noreferrer">
-        <img src={imagePath} alt="Resume."></img>
-      </a>
+      <div className="uk-flex uk-flex-center">
+        <a href={resumePdf} target="_blank" rel="noreferrer">
+          <img src={imagePath} alt="Resume."></img>
+        </a>
+      </div>
     </div>
   );
 }
