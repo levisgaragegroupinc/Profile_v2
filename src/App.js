ccfa3ad3585e52703422cc3287c91d0c466365d6
Travis Levi B <tbateswsu@gmail.com>
2022-07-20 15:16:54 -0700

    Code clean: Removed commented out code from app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,7 @@
 import React from "react";
 import "./App.css";
-// import About from "./components/About";
-// import NavBar from "./components/Navbar";
-// import Footer from "./components/Footer";
-// import TitleDivider from "./components/Dividers/Divider";
-// import Projects from "./components/Projects/Projects";
 import Portfolio from "./components/Portfolio";
 
 const App = () => <Portfolio />;
-
-// function App() {
-//   return (
-//     <div>
-//       <NavBar />
-//       <Projects />
-//       <About />
-//       <Footer />
-//     </div>
-//   );
-// }
 
 export default App;
