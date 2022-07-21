187424d51b82e0cc9ef90a33375601e18c03fd79
Travis Levi B <tbateswsu@gmail.com>
2022-07-21 10:29:15 -0700

    Refactor:  Phone regex was too strict, updated to accept more variations.

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,9 +1,12 @@
 export function emailValidation(email) {
-  const emailRegex = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+  const emailRegex = /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
   return emailRegex.test(String(email).toLowerCase());
 }
 
 export function phoneValidation(phone) {
-  const phoneRegex = /^\+?[0-9]{3}-?[0-9]{6,12}$/;
+  const phoneRegex =
+    /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
   return phoneRegex.test(String(phone));
 }
+
+// /^\+?[0-9]{3}-?[0-9]{6,12}$/;
